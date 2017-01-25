26841c3773a0b7098d8368b782131a47f8742d76
Vanesa93 <vns.georgieva@gmail.com>
2017-01-25 21:39:34 +0200

    Add missing files

---
+++ src/bg/fdiba/vgeorgieva/Messages.java
@@ -0,0 +1,65 @@
+package bg.fdiba.vgeorgieva;
+
+public class Messages {
+	private static String message;	
+
+	public static String getPause() {
+		message = "Pause";
+		return message;
+	}
+
+	public static String getResume() {
+		message = "Press S to resume";
+		return message;
+	}
+
+	public static String getPauseWithKey() {
+		message = "Pause P";
+		return message;
+	}
+
+	public static String getRestartWithK() {
+		message = "Restart R";
+		return message;
+	}
+
+	public static String getExitWithKey() {
+		message = "Exit Esc";
+		return message;
+	}
+
+	public static String getCurrentLevel() {
+		message = "Current level %d";
+		return message;
+	}
+
+	public static String getCollectTreasures() {
+		message = "Collect %d Treasures";
+		return message;
+	}
+
+	public static String getTreasuresCollected() {
+		message = "Treasures collected %d/%d";
+		return message;
+	}
+
+	public static String getScore() {
+		message = "Score %d";
+		return message;
+	}
+
+	public static String getGameOver() {
+		message = "Game Over";
+		return message;
+	}
+
+	public static String getRecord() {
+		message = "Record %d";
+		return message;
+	}
+
+	public static String getNewRecord() {
+		message = "New Record %d";
+		return message;
+	}
+	}
